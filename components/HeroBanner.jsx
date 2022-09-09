971c6e5b9c9e59007b6d76215293d014bcd3433e
joaop_almeida <96277609+joaop_almeida@users.noreply.github.com>
2022-09-09 15:09:31 -0300

    herobanner dimanicly added

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
-const HeroBanner = () => {
-  return (
-    <div>HeroBanner</div>
-  )
+
+const HeroBanner = ({ heroBanner }) => {
+    return (
+        <div className='hero-banner-container'>
+            <div>
+                <p className='beats-solo'>{heroBanner.smallText}</p>
+            </div>
+            <h3>#TODO:MID TEXT</h3>
+            <img src="" alt="headphones" className='hero-banner-image' />
+
+            <div>
+                <Link href="/Product/ID">
+                    <button type='button'>#TODO:BUTTON TEXT</button>
+
+                </Link>
+                <div className='desc'>
+                    <h5>Description</h5>
+                    <p>#TODO:DESCRIPTION</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default HeroBanner
