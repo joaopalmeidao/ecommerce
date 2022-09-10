dce7f4a61696c3ef0c924ee365551a7bd33e789d
joaop_almeida <96277609+joaop_almeida@users.noreply.github.com>
2022-09-10 18:18:13 -0300

    useStateContext added

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,11 +4,14 @@ import { client, urlFor } from '../../lib/client'
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import Product from '../../components/Product';
+import { userStateContext, useStateContext } from '../../context/StateContext'
+
 
 const ProductDetails = ({ product, products }) => {
 
     const { image, name, details, price, promotional_price } = product;
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty } = useStateContext()
 
     return (
         <div>
@@ -50,9 +53,9 @@ const ProductDetails = ({ product, products }) => {
                     <div className='quantity'>
                         <h3>Quantity:</h3>
                         <p className='quantity-desc'>
-                            <span className='minus' onClick=''><AiOutlineMinus /></span>
-                            <span className='num' onClick=''>#TODO</span>
-                            <span className='plus' onClick=''><AiOutlinePlus /></span>
+                            <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                            <span className='num' onClick=''>{qty}</span>
+                            <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className='buttons'>
