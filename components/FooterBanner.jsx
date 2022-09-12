3c1b5c14dc1b1d0da7c3f1aebb3e2028e6122534
joaop_almeida <96277609+joaop_almeida@users.noreply.github.com>
2022-09-12 00:09:35 -0300

    style changes

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 import { urlFor } from '../lib/client'
 
 const FooterBanner = ({ footerBanner: {
-  discount,largeText1,largeText2,saleTime,
-  smallText,midText,product,buttonText,image,desc
+  discount, largeText1, largeText2, saleTime,
+  smallText, midText, product, buttonText, image, desc
 } }) => {
   return (
     <div className='footer-banner-container'>
       <dvi className="banner-desc">
-        
+
         <div className="left">
           <p>{discount}</p>
           <h3>{largeText1}</h3>
@@ -29,9 +29,9 @@ const FooterBanner = ({ footerBanner: {
             </button>
           </Link>
         </div>
-        <img src={urlFor(image)} 
-        alt=""
-        className="footer-banner-image" />
+        <img src={urlFor(image)}
+          alt=""
+          className="footer-banner-image" />
       </dvi>
     </div>
   )
